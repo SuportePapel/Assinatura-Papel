3266652ebe5b4d4aae77a3dca5fe6ec71e285ce5
Anderson <anderson.scastilho@hotmail.com>
2022-07-22 17:01:58 -0300

    Criando form e ajustando layout

--- Assinatura/script/script.js
+++ Assinatura/script/script.js
@@ -1,19 +1,37 @@
-let name = document.querySelector("input[name = 'name']").value
-let celular = document.querySelector("input[name = 'celular']").value
-let cargo = document.querySelector("input[name = 'cargo']").value
-let ramal = document.querySelector("input[name = 'ramal']").value
-
 const form = document.querySelector(".form")
-const pNome = document.querySelector("p[name = 'nomeAlterar']")
-const pCargo  = document.querySelector("p [name ='cargoAlterar']")
-const pCelular = document.querySelector("p [ name = 'celAlterar']")
-const pRamal = document.querySelector("p[name = 'ramalAlterar']")
+let div = document.querySelector(".assinatura")
+let inputName = document.querySelector("input[name = 'name']")
+let inputCargo = document.querySelector("input[name = 'cargo']")
+let inputCelular = document.querySelector("input[name = 'celular']")
+let inputRamal = document.querySelector("input[name = 'ramal']")
 
 
-form.addEventListener('submit',(event)=>{
-    event.preventDefault()
-    addicionarNome()
-})
-function addicionarNome(){
-    console.log()
+let nomeAlterar = document.querySelector("p[name = 'nomeAlterar']")
+let cargoAlterar = document.querySelector("p[name = 'cargoAlterar']")
+let celularAlterar = document.querySelector("span[name = 'celularAlterar']")
+let ramalAlterar = document.querySelector("span[name = 'ramalAlterar']")
+
+    form.addEventListener('submit', (event)=>{
+        event.preventDefault()
+        adicionarNome()
+        adicionarCargo()
+        adicionarCelular()
+        adicionarRamal()
+    })
+
+function adicionarNome(){
+    nomeAlterar.innerHTML= inputName.value
+    inputName.value = ''
+}
+function adicionarCargo (){
+    cargoAlterar.innerHTML = inputCargo.value
+    inputCargo.value = ''
+}
+function adicionarCelular(){
+    celularAlterar.innerHTML = `${"Tel.: 55 11"} ${""} ${inputCelular.value}`
+    inputCelular.value = ''
+}
+function adicionarRamal (){
+    ramalAlterar.innerHTML = `${"Ramal:"} ${""} ${inputRamal.value}`
+    inputRamal.value = ''
 }
