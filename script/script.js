3213cec5328c6378efecb599868ed93291dab3fe
Anderson Leonardo <anderson.scastilho@hotmail.com>
2022-07-25 07:09:02 -0300

    Adicionando '-' o array

--- script/script.js
+++ script/script.js
@@ -33,8 +33,8 @@ function adicionarCargo (){
     inputCargo.value = ''
 }
 function adicionarCelular(){
-    let  array = [`${inputCelular.value[0]}${inputCelular.value[1]}${inputCelular.value[2]}${inputCelular.value[3]}${inputCelular.value[4]}${inputCelular.value[5]}${inputCelular.value[6]}${inputCelular.value[7]}`]
-
+    let  array = [`${inputCelular.value[0]}`+`${inputCelular.value[1]}`+ `${inputCelular.value[2]}`+`${inputCelular.value[3]}`+ `-`+`${inputCelular.value[4]}`+`${inputCelular.value[5]}`+`${inputCelular.value[6]}`+`${inputCelular.value[7]}`]
+    console.log(array)
     celularAlterar.innerHTML = `${"Tel.: 55 11"} ${""} ${array}`
     console.log(array)
     inputCelular.value = '' 
